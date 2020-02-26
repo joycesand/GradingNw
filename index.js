152e0f860284a05bd7bcadc088f08443c5d8df29
joycesand <ajoycesandra@gmail.com>
2020-02-26 13:44:16 +0300

    changes on index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const student = require("./models/student")
 const mark1 = new marks('SST', 'sandra', 80)
 // mark1.computeScore();
 
-const sandie = new marks('SST', 'joy', 84)
+const sandie = new marks('SST', 'joy', 85)
 // sandie.computeScore();
 
 module.exports = sandie;
